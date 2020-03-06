6f642057779460600cc589ae31920096d5575356
noranb <noranb@stud.ntnu.no>
2020-03-06 14:45:32 +0100

    added post request for rebooting system that hosts kodi, and quiting kodi application

--- python-scripts/kodi_post_request_ex.py
+++ python-scripts/kodi_post_request_ex.py
@@ -7,17 +7,23 @@ headers = {
 "Content-Type": "application/json"
 }
 
-data = {"jsonrpc": "2.0", "method": "Player.GetActivePlayers", "id": 1}
 
-# Pop up message 
+# Pop up message
 data_new_controller = {"jsonrpc":"2.0","method":"GUI.ShowNotification","params":{"title":"New Controller","message":"A new Controller was plugged in"},"id":1}
+data_json_new_controller = json.dumps(data_new_controller)
 
-data_json = json.dumps(data)
-
-r = requests.post(url = API_ENDPOINT, headers=headers, data = data_json)
-
-data_json = json.dumps(data_new_controller)
-
-r = requests.post(url = API_ENDPOINT, headers=headers, data = data_json)
-
+r = requests.post(url = API_ENDPOINT, headers=headers, data = data_json_new_controller)
 print(r.json())
+
+
+#quit application
+#data_close_kodi = {"jsonrpc":"2.0","method":"Application.Quit","id":1}
+#data_json_close_kodi = json.dumps(data_close_kodi)
+#r = requests.post(url = API_ENDPOINT, headers=headers, data = data_json_close_kodi)
+#print(r.json())
+
+#reboots system that runs kodi
+#data_reboot = {"jsonrpc":"2.0","method":"System.Reboot","id":1}
+#data_json_reboot = json.dumps(data_reboot)
+#r = requests.post(url = API_ENDPOINT, headers=headers, data = data_json_reboot)
+#print(r.json())
