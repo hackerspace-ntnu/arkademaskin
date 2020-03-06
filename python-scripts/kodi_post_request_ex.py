c30ea86837057193f516aa215f0f73fecab74711
noranb <noranb@stud.ntnu.no>
2020-03-06 11:49:02 +0100

    added the possiblity of sending e popup post request to kodi

--- python-scripts/kodi_post_request_ex.py
+++ python-scripts/kodi_post_request_ex.py
@@ -9,9 +9,15 @@ headers = {
 
 data = {"jsonrpc": "2.0", "method": "Player.GetActivePlayers", "id": 1}
 
+# Pop up message 
+data_new_controller = {"jsonrpc":"2.0","method":"GUI.ShowNotification","params":{"title":"New Controller","message":"A new Controller was plugged in"},"id":1}
+
 data_json = json.dumps(data)
 
 r = requests.post(url = API_ENDPOINT, headers=headers, data = data_json)
 
+data_json = json.dumps(data_new_controller)
+
+r = requests.post(url = API_ENDPOINT, headers=headers, data = data_json)
 
 print(r.json())
