48c528422aafa9d92412c5fbf118ada4d55014bb
noranb <noranb@stud.ntnu.no>
2020-03-09 18:42:50 +0100

    added code for opening plugin

--- python-scripts/kodi_post_request_ex.py
+++ python-scripts/kodi_post_request_ex.py
@@ -9,12 +9,27 @@ headers = {
 
 
 # Pop up message
-data_new_controller = {"jsonrpc":"2.0","method":"GUI.ShowNotification","params":{"title":"New Controller","message":"A new Controller was plugged in"},"id":1}
-data_json_new_controller = json.dumps(data_new_controller)
+#data_new_controller = {"jsonrpc":"2.0","method":"GUI.ShowNotification","params":{"title":"New Controller","message":"A new Controller was plugged in"},"id":1}
+#data_json_new_controller = json.dumps(data_new_controller)
 
-r = requests.post(url = API_ENDPOINT, headers=headers, data = data_json_new_controller)
+#r = requests.post(url = API_ENDPOINT, headers=headers, data = data_json_new_controller)
+#print(r.json())
+
+#open plugin and favorite shitt
+data_open_plugin_fav = {
+  "jsonrpc": "2.0",
+  "method": "GUI.ActivateWindow",
+  "id": 1,
+  "params": {
+    "window": "games",
+    "parameters": [
+     "plugin://plugin.program.advanced.emulator.launcher/?catID=vcategory_collections&com=SHOW_COLLECTION_ROMS&launID=19f283de98723e813d15a268f4fa2441"
+    ]
+  }
+}
+data_json_open_plugin_fav = json.dumps(data_open_plugin_fav)
+r = requests.post(url = API_ENDPOINT, headers=headers, data = data_json_open_plugin_fav)
 print(r.json())
-
 
 #quit application
 #data_close_kodi = {"jsonrpc":"2.0","method":"Application.Quit","id":1}
